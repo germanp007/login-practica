e0d268a8a00cde75ab508ae689e899d6a035bc10
German Pinto <93947560+germanp007@users.noreply.github.com>
2023-11-13 16:28:09 -0300

    Update dbConection.js

--- src/config/dbConection.js
+++ src/config/dbConection.js
@@ -5,8 +5,8 @@ export const conectionDB = async () => {
     await mongoose.connect(
       "mongodb+srv://germanp007:nati23032023@clustergap.ggconiz.mongodb.net/session?retryWrites=true&w=majority"
     );
-    console.log("Conectado a la Base de Datos Mongo!");
+    console.log("Conectado a la Base de Datos Mongo con exito!");
   } catch (error) {
-    console.log("Error al conectar a la Base de Datos");
+    console.log("Error al conectar a la Base de Datos, algunos datos estan mal");
   }
 };
